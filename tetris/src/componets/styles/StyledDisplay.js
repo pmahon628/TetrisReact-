10bc94bd11d52c3bdaf72419e9b5b308697e4b43
Patrick Mahon <p.mahon0628@gmail.com>
2019-09-26 22:33:13 -0400

    new styles

---
+++ tetris/src/componets/styles/StyledDisplay.js
@@ -0,0 +1,18 @@
+import style from 'styled-components';
+
+export const StyledDisplay  = styled.div`
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    margin: 0 0 20px 0;
+    padding: 20px;
+    border: 4px solid #333;
+    min-height: 30px;
+    width: 100%;
+    border-radius: 20pxl;
+    color: ${props => (props.gameOver ? 'red' : #999)};
+    background: #000;
+    font-family: Pixel, Arial, Helvectica, sans-serif;
+    font-size: 0.8rem;
+`
+;
