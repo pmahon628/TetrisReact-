40a0b7c55981c65e89a06c0b77caa70782198c58
Patrick Mahon <p.mahon0628@gmail.com>
2019-09-27 22:43:36 -0400

    work

--- tetris/src/componets/Tetris.js
+++ tetris/src/componets/Tetris.js
@@ -1,11 +1,15 @@
-import React [useState] from 'react';
+import React {useState} from 'react';
 
+import {createStage} from '../gameHelpers';
 
+// styled components
 import {StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
 
+// custom hooks
 import {usePlayer}  from '../hooks/usePlayer';
 import {useStage} from '../hooks/useStage';
 
+// components
 import Stage from './Stage';
 import Display from  './Display';
 import StartButton from './StartBtn';
@@ -17,10 +21,41 @@ const Tetris = () => {
    const [player] = usePlayer();
    const [stage, setStage] = useStage(player);
 
+const movePlayer = dir  => {
 
-  console.log('re-render');
-  return (
-    <StyledTetrisWrapper>
+
+}
+
+const startGame = () => {
+  // reset everything
+  setStage(createStage());
+}
+
+const drop = () => {
+  updatePlayerPos({ x: 0, y=1, collided:false})
+
+}
+  
+const dropPlayer = () => {
+  drop();
+}
+
+const move = ({ keyCode}) => {
+  if(!gameOver){
+   if (e.keyCode  === 37){
+      movePlayer(-1);
+    } else id (keyCode ===  39){
+      movePlayer(1)
+    }  else if (keyCode === 40){
+      dropPlayer();
+    }
+  }
+}
+  
+console.log('re-render');
+  
+return (
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
