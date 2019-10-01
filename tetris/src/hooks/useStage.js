5a2894ed0b3d5bd9c4baba945a54aa25875f4d5e
Patrick Mahon <p.mahon0628@gmail.com>
2019-09-30 22:52:22 -0400

    hook bug fixes

--- tetris/src/hooks/useStage.js
+++ tetris/src/hooks/useStage.js
@@ -1,35 +1,37 @@
-import {useState,  useEffect} from 'react';
-import {createStage} from '../gameHelpers';
-]
+import { useState, useEffect } from 'react';
+import { createStage } from '../gameHelpers';
+
 export const useStage = (player, resetPlayer) => {
-    const[stage, setStage] = useState(createStage());
+  const [stage, setStage] = useState(createStage());
 
-    useEffect(() => ){
-        const updateStage = prevState => {
-        // first flush the stage
-        const newStage = prev.State.map(row=>
-            row.map(cell => (cell)[1]  === 'clear' ? [0, 'clear'] : cell)),
-            ):
-        
-            // then draw the  tetromonio
-            player.tetromoni.forEach((row,  y) => {
-                row.forEach((value , x) => {
-                    if(vlaue !==0){
-                       newStage[y, + player.pos.y][x + player.pos.x]=[
-                           value,
-                           `${player.collided ? 'merged'  : 'clear'}`
-                       ];
-                    }
-                });
-            });
-            // then check  if we collided
-                if (player.collided){
-                    resetPlayer();
-                }
-            return newStage;
-        }:
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // First flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
 
-     setStage(prev => updateStage(prev));
-     }, [player, resetPlayer]);
-        return[stage, setStage];
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
+
+      return newStage;
+    };
+
+    setStage(prev => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage];
 };
